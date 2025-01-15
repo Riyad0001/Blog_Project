ebfd2054595a070090bf1bec4e73286247a41c48
Riyad0001 <rieadhasan499@gmail.com>
2025-01-16 02:22:47 +0600

    req file add

--- blog_project/settings.py
+++ blog_project/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env("Secret_key")
-CSRF_TRUSTED_ORIGINS = ['https://home-care-3sw7.onrender.com','httpps://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://five-minutes-blog.onrender.com','httpps://*.127.0.0.1']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
